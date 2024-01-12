aee2c9ee0baf10f60c73ac588e419d5954af72ff
celestialalpaca <denizgumus1996@hotmail.com>
2024-01-12 14:05:49 +0300

    Deneme3

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        int deniz = 4;
         System.out.println("Deneme123123123123");
         System.out.println("Deneme123123123123");
         System.out.println("Deneme123123123123");
         System.out.println("Deneme123123123123");
-        System.out.println("Deneme123123123123");
-        System.out.println("Deneme123123123123");
+
         System.out.println("Deneme123123123123");
         System.out.println("Deneme123123123123");
         System.out.println("Deneme123123123123");
