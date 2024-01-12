6bb0c6fb0858daf4f509be24cf45d9695a515fad
celestialalpaca <denizgumus1996@hotmail.com>
2024-01-12 13:49:54 +0300

    Deniz

--- Main.java
+++ Main.java
@@ -2,5 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Deneme123123123123");
+        System.out.println("Deneme123123123123");
+        System.out.println("Deneme123123123123");
+        System.out.println("Deneme123123123123");
     }
 }
