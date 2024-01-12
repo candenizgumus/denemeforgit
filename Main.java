b329a49a18679735a572b55a857a581860f573b9
celestialalpaca <denizgumus1996@hotmail.com>
2024-01-12 13:57:29 +0300

    Deneme3

--- Main.java
+++ Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println("Deneme123123123123");
         System.out.println("Deneme123123123123");
         System.out.println("Deneme123123123123");
+        System.out.println("Deneme123123123123");
     }
 }
